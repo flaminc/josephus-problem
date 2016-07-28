e1daaa963f34d8789df4ec024c8cb87d43c88b21
Chris Flaming <chris.flaming@gmail.com>
2016-07-28 18:05:35 -0500

    Added some scaladocs

--- src/main/scala/Josephus.scala
+++ src/main/scala/Josephus.scala
@@ -3,7 +3,13 @@ import scala.util.control.Exception.allCatch
 import utils.MathUtils.PositiveMod
 
 object Josephus {
-
+  /**
+    * Find the survivor's starting position after everyone else was eliminated.
+    *
+    * @param n The number of people in the circle.
+    * @param k Step rate. After skipping `k - 1` people, the `k`th person will be eliminated.
+    * @return The 0-based starting index of the last surviving person.
+    */
   def findSurvivor(n: Int, k: Int): Int = {
     if (n < 1) {
       throw new IllegalArgumentException("group size (n) must be 1 or greater")
@@ -37,6 +43,15 @@ object Josephus {
 
   }
 
+  /**
+    * Convenience method to cast command line arguments to a specified type and show errors on failures.
+    *
+    * @param argument   Argument from command line to parse/cast.
+    * @param castFn     Function to use for casting. Should throw exception when parsing/casting fails.
+    * @param failureMsg Message to use to clarify the error to the end user.
+    * @tparam a Type to cast argument to.
+    * @return Argument that was successfully casted to desired type `a`.
+    */
   def argCast[a](argument: String, castFn: String => a, failureMsg: String): a = {
     allCatch.opt(castFn(argument)) match {
       case Some(value) => value
@@ -46,6 +61,13 @@ object Josephus {
     }
   }
 
+  /**
+    * Kill the app and print `msg` provided to std error.
+    *
+    * @param msg       Description of why application is dieing to clarify to end user.
+    * @param exitCode  Will be the return code emitted on application death. Defaults to 1.
+    * @param showUsage Decides whether to show app usage on death.
+    */
   def die(msg: String, exitCode: Int = 1, showUsage: Boolean = false): Unit = {
     Console.err.println(s"Application died with error: $msg")
     if (showUsage) {
--- src/main/scala/utils/MathUtils.scala
+++ src/main/scala/utils/MathUtils.scala
@@ -1,8 +1,18 @@
 package utils
 
+/**
+  * Provide some basic math utils
+  */
 object MathUtils {
 
   implicit class PositiveMod(dividend: Int) {
+    /**
+      * Customize modulus operator `%` so that when the dividend is - the remainder is positive. No guarantees will be
+      * made about the divisor being negative.
+      *
+      * @param divisor The divisor in the division.
+      * @return Positive remainder of division.
+      */
     def positiveMod(divisor: Int): Int = {
       // Math.floorMod could have been used but that would peg java at 1.8
       if (dividend < 0) {
